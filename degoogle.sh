ce877a1bae6076e67398c1c3ccfe306074ae95e7
MahanRahmati <mahan.rahmati@protonmail.com>
2022-01-29 14:26:27 +0330

    Add shell script

---
+++ degoogle.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+adb shell settings put global captive_portal_https_url https://e.foundation/net_204/
+adb shell settings put global captive_portal_http_url http://204.ecloud.global
