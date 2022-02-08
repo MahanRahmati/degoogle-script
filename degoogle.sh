87beccc736476d48d61a04d56fd6ae5e064a6aa9
MahanRahmati <mahan.rahmati@protonmail.com>
2022-02-08 23:35:51 +0330

    Add captiveportal.kuketz.de

--- degoogle.sh
+++ degoogle.sh
@@ -2,3 +2,5 @@
 
 adb shell settings put global captive_portal_https_url https://e.foundation/net_204/
 adb shell settings put global captive_portal_http_url http://204.ecloud.global
+adb shell settings put global captive_portal_fallback_url http://captiveportal.kuketz.de
+adb shell settings put global captive_portal_other_fallback_urls http://captiveportal.kuketz.de
