d7946b70d3d4c4a7db098c8f1a9fc75406abaf70
MahanRahmati <mahan.rahmati@protonmail.com>
2022-02-09 08:00:08 +0330

    Add NTP

--- degoogle.sh
+++ degoogle.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
+# Captive portal
 adb shell settings put global captive_portal_https_url https://e.foundation/net_204/
 adb shell settings put global captive_portal_http_url http://204.ecloud.global
 adb shell settings put global captive_portal_fallback_url http://captiveportal.kuketz.de
 adb shell settings put global captive_portal_other_fallback_urls http://captiveportal.kuketz.de
+
+# NTP
+adb shell settings put global ntp_server asia.pool.ntp.org
